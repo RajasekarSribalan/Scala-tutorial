52c28e07942d290d78c7ad1791bd624a4c4e1f11
Rajasekar <emailtoraj90@gmail.com>
2018-07-21 11:00:27 +0530

    My First Scala app

---
+++ FirstScalaApp/build.sbt
@@ -0,0 +1,9 @@
+name := "FirstScalaApp"
+
+version := "0.1"
+
+scalaVersion := "2.11.6"
+
+
+EclipseKeys.withSource := true
+EclipseKeys.withJavadoc := true
---
+++ FirstScalaApp/src/main/scala/OrderRevenue.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+object OrderRevenue {
+
+  def main(args: Array[String]) = {
+
+    val orderItems = Source.fromFile("/home/hduser/itversity_data/data/retail_db/order_items/part-00000").getLines();
+
+    val orderItemsFilter = orderItems.filter(eachOrder => eachOrder.split(",")(1).toInt == 2)
+    
+    val orderItemsMap = orderItemsFilter.map(orderItem => orderItem.split(",")(4).toFloat)
+    
+    val outPut = orderItemsMap.reduce((sum,eachOrderAmount) => sum + eachOrderAmount)
+    
+    print(outPut);
+  }
+
+}
