8544d5eb2ebde51ffeb5341f75376f90fb8d5594
Rajasekar <emailtoraj90@gmail.com>
2018-07-21 11:04:51 +0530

    My First Scala app 1

--- FirstScalaApp/src/main/scala/OrderRevenue.scala
+++ FirstScalaApp/src/main/scala/OrderRevenue.scala
@@ -3,10 +3,12 @@ import scala.io.Source
 object OrderRevenue {
 
   def main(args: Array[String]) = {
+    
+    val orderId = args(1).toInt;
 
     val orderItems = Source.fromFile("/home/hduser/itversity_data/data/retail_db/order_items/part-00000").getLines();
 
-    val orderItemsFilter = orderItems.filter(eachOrder => eachOrder.split(",")(1).toInt == 2)
+    val orderItemsFilter = orderItems.filter(eachOrder => eachOrder.split(",")(1).toInt == orderId)
     
     val orderItemsMap = orderItemsFilter.map(orderItem => orderItem.split(",")(4).toFloat)
     
